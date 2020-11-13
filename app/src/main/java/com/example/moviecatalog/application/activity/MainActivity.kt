72db45de733f3203557423401a12aa22fa4bf3c5
arthealt <igar-12@ya.ru>
2020-11-14 02:33:48 +0300

    Add Search View

--- app/src/main/java/com/example/moviecatalog/application/activity/MainActivity.kt
+++ app/src/main/java/com/example/moviecatalog/application/activity/MainActivity.kt
@@ -1,14 +1,17 @@
-package com.example.moviecatalog
+package com.example.moviecatalog.application.activity
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
+import com.example.moviecatalog.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        toolbar.title = "Test"
     }
 }
